1a050961d82bde2a5d39bb3258adf236ba22e75d
andluo <1455234749@qq.com>
2019-01-24 16:32:53 +0800

    加上字体图标

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function $(selector){
     return document.querySelector(selector);
 }
 
-$('#list_btn').addEventListener('click',()=>{
-    $('#song_list').classList.toggle('show')
+// 音乐列表的展示
+list_btn.addEventListener('click',()=>{
+    song_list.classList.toggle('show')
 })
