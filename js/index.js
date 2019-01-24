204192596d00a5bf0f27cc78947a873144201d1c
andluo <1455234749@qq.com>
2019-01-24 20:56:50 +0800

    能正常播放上一首和下一首

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function $(selector){
     return document.querySelector(selector);
 }
 
+let music_list
 let music =  new Audio()
 music.autoplay = true
 let music_index = 0
@@ -14,7 +15,7 @@ xhr.addEventListener('load',()=>{
     if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
         console.log("音乐加载成功.")
         console.log(xhr.responseText)
-        let music_list = JSON.parse(xhr.responseText)
+        music_list = JSON.parse(xhr.responseText)
         load_music(music_list[music_index])
         
     }else{
@@ -32,19 +33,48 @@ function load_music(obj){
     music.src = obj.src
     cur_song_name.innerText = obj.title
     singer.innerText = obj.auther
+    play_or_stop.href.baseVal = "#icon-zanting"
+
 }
 
-
+//播放/暂停
 play_or_stop.addEventListener('click',()=>{
     if(music.paused){
         play_or_stop.href.baseVal = "#icon-zanting"
         music.play()    
+        console.log(`点击播放成功`)
     }else{
         play_or_stop.href.baseVal = "#icon-bofang"
         music.pause()
+        console.log(`点击暂停成功`)
+
 
     }
 })
+
+//播放下一首
+play_next.addEventListener('click',()=>{
+    if(music_index === music_list.length-1){
+        music_index = 0
+        load_music(music_list[music_index])
+    }else{
+        music_index++
+        load_music(music_list[music_index])
+    }
+})
+
+//播放上一首
+play_pre.addEventListener('click',()=>{
+    if(music_index === 0){
+        music_index = music_list.length -1
+        load_music(music_list[music_index])
+    }else{
+        music_index--
+        load_music(music_list[music_index])
+    }
+
+})
+//
 
 
 
